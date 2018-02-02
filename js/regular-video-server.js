53a54991722e23ff0387ece90aa499936e2fb09f
Akinyele Akintola-Febrissy <aakin013@uottawa.ca>
2018-02-02 13:30:12 -0500

    Using token checker on regular server

--- js/regular-video-server.js
+++ js/regular-video-server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var logging = require('./logging');
 var config = require('../tanoshimu-config');
+var tokenChecker = require('./token-checker');
 var app = express();
 
 app.use('/videos', express.static(config.defaultPath));
+app.use(tokenChecker);
 logging.log('All files are statically served on ' + config.defaultPath);
 
 module.exports = app;
