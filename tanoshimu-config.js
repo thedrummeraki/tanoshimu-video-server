f611ad1e209337f01d20a28ca1a07107ddad202d
Akinyele Akintola-Febrissy <akinyele.akintola.febrissy@gmail.com>
2018-02-27 10:53:03 -0500

    Update tanoshimu-config.js

--- tanoshimu-config.js
+++ tanoshimu-config.js
@@ -1,9 +1,11 @@
+var ssl_domain = process.env.TANOSHIMU_DOMAIN || "test.com";
+
 module.exports = {
     defaultVideoPath: "./assets/anime-404-not-found.mp4",
     defaultImagePath: "./assets/anime-404-not-found.jpg",
     defaultErrorImagePath: "./assets/error-icon.png",
     server_type: process.env.TANOSHIMU_SERVER_TYPE || 'inline',
-    tanoshimu_ssl_directory: process.env.TANOSHIMU_SSL_DIRECTORY || '/etc/letsencrypt/live/tanoshimu.akinyele.ca/',
+    tanoshimu_ssl_directory: process.env.TANOSHIMU_SSL_DIRECTORY || '/etc/letsencrypt/live/'.concat(ssl_domain),
     enable_ssl: (process.env.TANOSHIMU_ENABLE_SSL || 'false') == 'true',
     enable_ssl_redirect: (process.env.TANOSHIMU_ENABLE_SSL_REDIRECT || 'false') == 'true',
     http_port: process.env.TANOSHIMU_HTTP_PORT || 3000,
